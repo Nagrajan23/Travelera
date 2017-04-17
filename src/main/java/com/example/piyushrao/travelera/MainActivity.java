db2e9739c6aa315ed021a1e7f057a927ad5a5dbc
Nagrajan23 <c.raj.23@gmail.com>
2017-04-17 19:25:33 -0400

    Piyush Base Changes

---
+++ src/main/java/com/example/piyushrao/travelera/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.piyushrao.travelera;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button search;
+    public Button nearby;
+
+    public void init(){
+        search= (Button) findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent search= new Intent(MainActivity.this, SearchActivity.class);
+                        startActivity(search);
+                    }
+                });
+
+        nearby= (Button) findViewById(R.id.nearby);
+        nearby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nearby= new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(nearby);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+}
---
+++ src/main/java/com/example/piyushrao/travelera/MapsActivity.java
@@ -0,0 +1,70 @@
+package com.example.piyushrao.travelera;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        SQLiteDatabase db;
+        db = openOrCreateDatabase("TestingData.db",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        db.setVersion(1);
+        db.setLocale(Locale.getDefault());
+
+        Cursor cursor1 = db.rawQuery("SELECT * FROM TourSites1", null);
+        cursor1.moveToFirst();
+
+        double lat,lon;
+        String place;
+        LatLng liberty = new LatLng(0,0);
+        do
+        {
+            lat = cursor1.getDouble(2);
+            lon = cursor1.getDouble(3);
+            place = cursor1.getString(1);
+            liberty = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(liberty).title(place));
+        }while(cursor1.moveToNext());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(liberty));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+        // TODO http://stackoverflow.com/questions/17061063/map-search-bar-in-google-maps-v2#27243187
+    }
+}
