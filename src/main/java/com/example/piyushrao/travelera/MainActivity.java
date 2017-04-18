58cd86f4449dbef6e99f74ca3a1e67e0ffc9e407
Nagrajan23 <c.raj.23@gmail.com>
2017-04-18 17:44:41 -0400

    Maps with DatabaseWorking

--- src/main/java/com/example/piyushrao/travelera/MainActivity.java
+++ src/main/java/com/example/piyushrao/travelera/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.piyushrao.travelera;
 //Test Nagrajan Remote
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,43 @@ public class MainActivity extends AppCompatActivity {
     public Button nearby;
 
     public void init(){
+
+        //Create Database
+        SQLiteDatabase db;
+        db = openOrCreateDatabase("TestingData.db",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        db.setVersion(1);
+        db.setLocale(Locale.getDefault());
+
+        final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE IF NOT EXISTS " + "TourSites1" + " (" +
+                        "_id" + " INTEGER PRIMARY KEY," +
+                        "siteName" + " TEXT," +
+                        "category" + " TEXT," +
+                        "category2" + " TEXT," +
+                        "latitude" + " DOUBLE," +
+                        "longitude" + " DOUBLE," +
+                        "rating" + " FLOAT)";
+        db.execSQL(SQL_CREATE_ENTRIES);
+        //Create initial Records
+        Cursor cursor1 = db.rawQuery("SELECT * FROM TourSites1", null);
+        if(cursor1.getCount() == 0)
+        {
+            db.execSQL("insert into TourSites1 values(1201,'Statue of Liberty','manMade',' ',40.689246, -74.044499,0)");
+            db.execSQL("insert into TourSites1 values(1202,'Central Park','park',' ',40.782872, -73.965357,0)");
+            db.execSQL("insert into TourSites1 values(1203,'Times Square','manMade',' ',40.758911, -73.985142,0)");
+            db.execSQL("insert into TourSites1 values(1204,'Yankee Stadium','manMade',' ',40.829735, -73.926144,0)");
+            db.execSQL("insert into TourSites1 values(1205,'One World Observatory','manMade',' ',40.713344, -74.013389,0)");
+            db.execSQL("insert into TourSites1 values(1206,'Central Park Zoo','wildlife',' ',40.768441,-73.970542,0)");
+            db.execSQL("insert into TourSites1 values(1207,'Empire State building','manMade',' ',40.748457,-73.985702,0)");
+            db.execSQL("insert into TourSites1 values(1208,'Trinity Church','religious',' ',40.708065,-74.012185,0)");
+            db.execSQL("insert into TourSites1 values(1209,'Battery park','park','waterfront',40.702486,-74.016770,0)");
+            db.execSQL("insert into TourSites1 values(1210,'Rockefeller Center','manMade',' ',40.758807,-73.978803,0)");
+            db.execSQL("insert into TourSites1 values(1211,'Teardrop park','park','waterfront',40.717068,-74.015532,0)");
+            db.execSQL("insert into TourSites1 values(1212,'Charging Bull','manMade',' ',40.705523,-74.013457,0)");
+            db.execSQL("insert into TourSites1 values(1213,'American Museum of natural history','manMade','museum',40.781328,-73.973988,0)");
+        }
+        db.close();
+
         search= (Button) findViewById(R.id.search);
         search.setOnClickListener(new View.OnClickListener() {
                     @Override
--- src/main/java/com/example/piyushrao/travelera/MapsActivity2.java
+++ src/main/java/com/example/piyushrao/travelera/MapsActivity2.java
@@ -1,5 +1,7 @@
 package com.example.piyushrao.travelera;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Locale;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +43,29 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        SQLiteDatabase db;
+        db = openOrCreateDatabase("TestingData.db",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        db.setVersion(1);
+        db.setLocale(Locale.getDefault());
+
+        Cursor cursor1 = db.rawQuery("SELECT * FROM TourSites1", null);
+        cursor1.moveToFirst();
+
+        double lat,lon;
+        String place;
+        LatLng liberty = new LatLng(0,0);
+        do
+        {
+            lat = cursor1.getDouble(4);
+            lon = cursor1.getDouble(5);
+            place = cursor1.getString(1);
+            liberty = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(liberty).title(place));
+        }while(cursor1.moveToNext());
+        db.close();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(liberty));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+        // TODO http://stackoverflow.com/questions/17061063/map-search-bar-in-google-maps-v2#27243187
     }
 }
