37cf8da24057a80847f5cf86ab22e1373bb153d1
Nagrajan23 <c.raj.23@gmail.com>
2017-04-27 21:52:43 -0400

    Fixed some cosmetic issue

--- src/main/java/com/example/piyushrao/travelera/LoginActivity.java
+++ src/main/java/com/example/piyushrao/travelera/LoginActivity.java
@@ -3,9 +3,13 @@ package com.example.piyushrao.travelera;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +31,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText editName1 = (EditText) findViewById(R.id.editName);
+        editName1.setInputType(InputType.TYPE_CLASS_TEXT);
+        editName1.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId,
+                                          KeyEvent event)
+            {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    // Handle pressing "Enter" key here
+                    onClickEnter();
+
+                    handled = true;
+                }
+                return handled;
+            }
+        });
         login();
     }
 
